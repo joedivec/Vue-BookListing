246903d89145206aaa5a0065cc8f9575e31f370f
Joe DiVecchio <joedivec@tampabay.rr.com>
2020-02-28 15:40:43 -0500

    Update BookList.vue

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -14,9 +14,9 @@ export default {
       return {
         title: 'All Books',
         books: [
-          {title: 'Self Reliance', author: 'Ralph Waldo Emerson'},
-          [title: 'American Gods', author: 'Neil Gaiman'],
-          [title: 'Amusing Ourselves to Death', author: 'Neil Postman']
+            {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+            {title: 'American Gods', author: 'Neil Gaiman'},
+            {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
         ]
       }
     }
