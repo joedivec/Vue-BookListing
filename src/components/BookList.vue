d9ca2ffefa8f4137102a0dadc2069156c42725a8
Joe DiVecchio <joedivec@tampabay.rr.com>
2020-02-28 14:51:53 -0500

    Add BookList
    Add BookList

---
+++ src/components/BookList.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <h1>{{ title }}
+</div>
+</template>
+
+<script>
+export default {
+    name: 'book-list',
+    data () {
+      return {
+        title: 'All Books'
+      }
+    }
+}
+</script>
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
