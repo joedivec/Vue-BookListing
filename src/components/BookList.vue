8ef02ee9e1ac5b91649246029bf7385cc85556dd
Joe DiVecchio <joedivec@tampabay.rr.com>
2020-02-28 16:52:59 -0500

    Update BookList.vue

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -30,7 +30,7 @@ export default {
       }
     },
     methods: {
-      appendBook(bookTitle, bookAuthor {
+      appendBook(bookTitle, bookAuthor) {
         this.books.push({ title: bookTitle, author: bookAuthor })
       }
     }
