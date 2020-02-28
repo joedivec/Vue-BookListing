a000e8b24b9b6af4be936e687caac77660e87e55
Joe DiVecchio <joedivec@tampabay.rr.com>
2020-02-28 16:17:28 -0500

    Update BookItem.vue

--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -1,14 +1,12 @@
 <template>
     <li>{{ book.title }}:{{ book.author }}</li>
 </template
-
 <script>
 export default {
   name: 'BookItem',
   props: ['book']
 };
 </script>
-
 <style>
 li {
   display: block;
