0264f9ea7f7b5c3b48ed7cd50ccd52145cb958e1
Joe DiVecchio <joedivec@tampabay.rr.com>
2020-02-28 16:35:03 -0500

    Create BookForm.vue

---
+++ src/components/BookForm.vue
@@ -0,0 +1,14 @@
+<template></template>
+<script>
+export default {
+  name: 'BookForm',
+  props: ['Books'],
+  data () {
+    return {
+      bookTitle: '',
+      bookAuthor: ''
+    }
+  }
+};
+</script>
+<style></style>
